35674c7c9615922ad9ada59daa710cb2b7c177d7
Achmad Furqon Rachmadie <uuqkun@gmail.com>
2024-04-17 22:36:37 +0700

    vercel: API test

--- src/route/public-api.js
+++ src/route/public-api.js
@@ -3,10 +3,9 @@ import userController from "../controller/user-controller.js";
 
 const publicRouter = new express.Router();
 
-publicRouter.get("/api", (req, res) => {
-  res.status(200).json({ message: "Welcome to the MTF API" });
-});
-publicRouter.post("/api/auth/register", userController.register);
-publicRouter.post("/api/auth/login", userController.login);
+publicRouter.post('/api/auth/register', userController.register); 
+publicRouter.post('/api/auth/login', userController.login); 
 
-export { publicRouter };
+export {
+    publicRouter
+}
